56f912fd228450b8f63d4de2d994ec0b1d8156cb
StevenLiu <stevenliu986@gmail.com>
2023-09-12 22:30:56 +1000

    remove bodyParser

--- server/express.js
+++ server/express.js
@@ -14,7 +14,6 @@
  */
 
 import express from "express";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compress from "compression";
 import cors from "cors";
