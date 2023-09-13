a0e4db33d7d3f5ee4c715719fbd49c151c9d6358
StevenLiu <stevenliu986@gmail.com>
2023-09-13 21:44:02 +1000

    add new controllers

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model";
+import bcrypt from "bcrypt";
 
 const getUserList = async (req, res) => {
   try {
@@ -11,37 +12,50 @@ const getUserList = async (req, res) => {
 
 const addNewUser = async (req, res) => {
   const { name, email, password } = req.body;
-  const user = await User.insertMany({ name, email, password });
+  const user = new User({ name, email, password });
+  await user.encryptPassword();
+  await user.save();
   return res.status(200).json(user);
+};
+
+const signIn = async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email }).exec();
+  if (!existingUser) {
+    return res.status(401).json({ error: "Invalid email or password!" });
+  }
+  const isPasswordValid = await existingUser.authenticate(password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: "Invalid email or password!" });
+  }
+  // 通常情况下要在这里向前端发个token
+  return res
+    .status(200)
+    .json({ name: existingUser.name, email: existingUser.email });
 };
 
 const updateUser = async (req, res) => {
   const { id } = req.params;
   const { email } = req.body;
-  //   const user = await User.find({ id }).exec();
-  //   if (!user) {
-  //     return res.status(404).json({ message: "User not found." });
-  //   }
-  try {
-    const user = await User.findByIdAndUpdate({ id }, { email: email }).exec();
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: "User not found and update failed." });
-    }
-    return res.status(200).json(user);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
+  console.dir(User);
+  const user = await User.findByIdAndUpdate(
+    { _id: id },
+    { email: email }
+  ).exec();
+  if (!user) {
+    return res.status(400).json({ message: "User update failed." });
   }
+  return res.status(200).json(user);
 };
 
-const deleteUser = async (req, res) => {
-  const { id } = req.params;
-  try {
-    await User.findOneAndDelete({ id }).exec();
-    return res.status(204).json({ message: "User deleted" });
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-export { getUserList, addNewUser, updateUser, deleteUser };
+// const deleteUser = async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     await User.findOneAndDelete({ id }).exec();
+//     return res.status(204).json({ message: "User deleted" });
+//   } catch (error) {
+//     return res.status(500).json({ message: error.message });
+//   }
+// };
+
+export { getUserList, addNewUser, signIn, updateUser };
