6719d25b30172134ae7b7236ffb2e0e40ce67910
StevenLiu <stevenliu986@gmail.com>
2023-09-13 21:42:08 +1000

    add new route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,15 +2,17 @@ import { Router } from "express";
 import {
   getUserList,
   addNewUser,
+  signIn,
   updateUser,
-  deleteUser,
+  // deleteUser,
 } from "../controller/user.controller";
 
 const userRouter = Router();
 
 userRouter.get("/users", getUserList);
 userRouter.post("/users", addNewUser);
+userRouter.post("/users/signin", signIn);
 userRouter.put("/users/:id", updateUser);
-userRouter.delete("/users/:id", deleteUser);
+// userRouter.delete("/users/:id", deleteUser);
 
 export default userRouter;
