acda272bda910d75bf1f54919eb4f08b69608ce1
StevenLiu <stevenliu986@gmail.com>
2023-09-15 21:02:24 +1000

    add errorHandler middleware

--- server/express.js
+++ server/express.js
@@ -28,5 +28,13 @@ app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: err.name + ": " + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ": " + err.message });
+    console.log(err);
+  }
+});
 
 export default app;
